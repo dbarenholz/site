1fabd7399211d71f4e2816ac22677455ce260d4c
dbarenholz <dbarenholz@users.noreply.github.com>
2022-05-14 11:27:21 +0000

    jekyll build from Action 810584f5b52d7bb11c6523f465a72d1752386cb4

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,31 @@
+var store = [{
+        "title": "Welcome!",
+        "excerpt":"Welcome to my blog!   Hopefully I’ll manage to find some time to write some posts. You can find my thoughts on practising piano, fun stories about research, my notes on learning something new, and more here!  ","categories": ["information"],
+        "tags": [],
+        "url": "/information/2021/12/18/welcome.html",
+        "teaser": null
+      },{
+        "title": "Site",
+        "excerpt":"Personal site, made using markdown files, Jekyll and Minimal Mistakes. There’s not much more to say, since you’re already browsing it.   Source Code  ","categories": [],
+        "tags": ["code"],
+        "url": "/projects/code/site/",
+        "teaser": null
+      },{
+        "title": "Sokkyou",
+        "excerpt":"An album titled Sokkyou, the romanized version of the Japanese word 即興 meaning improvisation, impromptu. It will consist of all pieces that I recorded while improvising. These pieces are commonly around one (1) to two (2) minutes in length. Right now, there are five (5) Sokkyous.  ","categories": [],
+        "tags": ["music"],
+        "url": "/projects/music/sokkyou/",
+        "teaser": null
+      },{
+        "title": "Travel",
+        "excerpt":"An album titled travel, consisting of three (3) pieces. I envision this as a huge project, where I (or my good friend Jakub) film music videos that are taken when, well, travelling. The pieces themselves are: Departure. This piece attempts to capture the feeling of “stress” or “the unknown”, but...","categories": [],
+        "tags": ["music"],
+        "url": "/projects/music/travel/",
+        "teaser": null
+      },{
+        "title": "Probabilistic Models & Process Discovery",
+        "excerpt":"Read the complete report here. Main Idea Process discovery (PD) in process mining is the task of transforming an event log into an interpretable model, usually a Petri net. Traditionally this transformation is achieved in an algorithmic sense, for instance using the Alpha algorithm. The event log, which is the...","categories": [],
+        "tags": ["research"],
+        "url": "/projects/research/internship-pa/",
+        "teaser": null
+      }]
